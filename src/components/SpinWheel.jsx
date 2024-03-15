e06685e57799f20a8be2119a4cbfc1ddb9d66e67
Gaspard Hedde <gaspard_hedde@yahoo.com>
2024-03-15 15:24:21 +0100

    BIG MAJ, V2

--- src/components/SpinWheel.jsx
+++ src/components/SpinWheel.jsx
@@ -1,55 +1,26 @@
-import React, { useState } from 'react';
-import './css/wheel.css';
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./css/wheel.css";
+import ConfigurePage from "./ConfigurePage";
+import RoulettePage from "./RoulettePage";
 
-const Wheel = () => {
-    const [fields, setFields] = useState([{ id: 1, name: '' }]);
-    const [result, setResult] = useState(null);
-  
-    const handleAddField = () => {
-      const newField = { id: fields.length + 1, name: '' };
-      setFields(prevFields => [...prevFields, newField]);
-    };
+import { Switch } from "react-router-dom";
 
-    const handleRemoveField = () => {
-        if (fields.length > 1) {
-          setFields(prevFields => prevFields.slice(0, -1));
-        }
-    };
-  
-    const handleFieldNameChange = (id, value) => {
-      setFields(prevFields =>
-        prevFields.map(field => (field.id === id ? { ...field, name: value } : field))
-      );
-    };
-  
-    const handleStartSpin = () => {
-      // Logique pour tourner la roue et afficher le résultat
-      const randomResult = Math.floor(Math.random() * fields.length);
-      setResult(randomResult);
-    };
-  
-    return (
-      <div className="roulette-container">
-        <h2>Wheel Spinner</h2>
-        <div className="fields-container">
-          {fields.map(field => (
-            <input
-              key={field.id}
-              type="text"
-              placeholder={`Champ ${field.id}`}
-              value={field.name}
-              onChange={e => handleFieldNameChange(field.id, e.target.value)}
-            />
-          ))}
-        </div>
-        <button onClick={handleAddField}>Ajouter un champ</button>
-        <button onClick={handleRemoveField}disabled={fields.length < 2}>Supprimer un champ</button>
-        <button onClick={handleStartSpin} disabled={fields.length < 2}>Lancer la roue</button>
-        {result !== null && (
-          <p>Résultat: {fields[result].name}</p>
-        )}
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/roulette">
+            <RoulettePage />
+          </Route>
+          <Route path="/">
+            <ConfigurePage />
+          </Route>
+        </Switch>
       </div>
-    );
-  };
-  
-export default Wheel;
+    </Router>
+  );
+}
